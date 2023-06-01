cdee041a21ba716e84c343633cff941bbeff14a9
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-06-01 21:27:34 +0300

    Done

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Module2HW3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var starter = new Starter();
+            starter.Run();
+        }
+    }
+}
